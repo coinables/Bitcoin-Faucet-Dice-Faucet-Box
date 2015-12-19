1e695b5800b99002381b7bfc50443103925e45d7
coinableS <coinables@gmail.com>
2015-12-19 10:25:18 -0800

    AreYouAHuman
    change to areyouahuman bot protection

---
+++ faucetbox/ayah_config.php
@@ -0,0 +1,13 @@
+<?php
+// Edit the two lines below to use the keys for your site.
+// (Note: you can find your keys at http://portal.areyouahuman.com/dashboard)
+define( 'AYAH_PUBLISHER_KEY', 'your_publisher_key_goes_here');
+define( 'AYAH_SCORING_KEY', 'your_scoring_key_goes_here');
+
+
+// Set defaults for values needed by the ayah.php file.
+// (Note: you do not need to change these.)
+define( 'AYAH_WEB_SERVICE_HOST', 'ws.areyouahuman.com');
+define( 'AYAH_TIMEOUT', 0);
+define( 'AYAH_DEBUG_MODE', FALSE);
+define( 'AYAH_USE_CURL', TRUE);
--- faucetbox/index.php
+++ faucetbox/index.php
@@ -10,11 +10,11 @@
 	$startBal = 300; //starting balance for users
 	$reefAmount = 300; //referral amount
 	$timeBetweenClaims = 1800; //wait time between claims in seconds
-	$private_key = "1a1a1a_1a1a1a_1a_1a1"; //funcaptcha private key
-	$publicKey = "1b1b1_1b1b_1b1b1b1"; //funcaptcha public key
-	
-	
+	require_once("ayah.php"); //Are You A Human Bot Protection insert your keys in the ayah_config file
+	                          //Sign up for a free account at areyouahuman.com to get your keys
 	require_once("faucetbox.php");
+	
+	$ayah = new AYAH();
 	$reefer = $_GET['ref'];
 	mysqli_set_charset($conn,"utf8");
 	$reefer = mysqli_real_escape_string($conn, $reefer);
@@ -30,13 +30,11 @@
 		$faucetBal = $faucetbox->getBalance();
 		$getfaucetbal2 = $faucetBal["balance_bitcoin"];
 
-	if(isset($_POST['start']) && !empty($_POST['start']))
+	if(array_key_exists('start', $_POST))
 {
 		$time = time();
-		
-		$session_token = $_POST["fc-token"];
-		$fc_api_url = "https://funcaptcha.com/fc/v/?private_key=".$private_key."&session_token=".$session_token."&simple_mode=1";
-		if (file_get_contents($fc_api_url) === "1") 
+		$score = $ayah->scoreResult();
+		if ($score)  
 	{
 		$userAddy = $_POST['bitad'];
 		mysqli_set_charset($conn,"utf8");
@@ -328,7 +326,6 @@ a {
 }
 
 </style>
-<script src="https://funcaptcha.com/fc/api/" async defer></script>
 </head>
 <body>
 <br><div id="user"><img src="logo.png"></div>
@@ -345,7 +342,7 @@ a {
 		  </div> <br>
 		<center><?php echo "<span id='error'>".$message."</span>"; echo "<span id='msg'>".$prizeMsg."</span>"; ?></center><br>
  <form method="post"><center>
-    <div id="funcaptcha" data-pkey="<?php echo $publicKey; ?>"></div>
+    <?php  echo $ayah->getPublisherHTML(); ?>
 	--AD SPOT--
 <input type="text" name="bitad" id="bitad" size="65" placeholder="Your BTC Address"><br>
  <input type="submit" name="start" id="claimBtn" value="PLAY"><br><br></form>
